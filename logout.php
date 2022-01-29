d5fc4b921d519de8c69eab9844490d06f31cf2ee
Nora Leonte <nora.leonte@evozon.com>
2022-01-29 13:49:03 +0200

    add database

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['name']);
-unset($_SESSION['userEmail']);
-unset($_SESSION['userURL']);
-header('Location: index.php');
--- utilities/setStatus.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('../includes/autoload.php');
-if (!empty($_POST['url']) && !empty($_POST['status'])) {
-    $user = new Users();
-    $thisUser = $user->getUserbyURL($_POST['url']);
-    if ($thisUser) {
-        $user->changeAccountStatus($thisUser['id'], $_POST['status']);
-    }
-}
